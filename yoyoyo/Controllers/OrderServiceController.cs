dc82a30d8e23b9b866acdc429fa1917a700ff1cf
yocheckman <op3511789@yahoo.com.tw>
2018-03-29 16:17:59 +0800

    建立屬性

---
+++ yoyoyo/Controllers/OrderServiceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace yoyoyo.Controllers
+{
+    public class OrderServiceController : Controller
+    {
+        // GET: OrderService
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ yoyoyo/Models/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace yoyoyo.Models
+{
+    public class Orders
+    {
+        [DisplayName("訂單編號")]
+        public int OrderID { get; set; }
+        [DisplayName("客戶編號")]
+        public int CustomerID { get; set; }
+        [DisplayName("員工編號")]
+        public int EmployeeID { get; set; }
+
+        [DisplayName("訂單日期")]
+        public DateTime OrderDate { get; set; }
+        [DisplayName("需要日期")]
+        public DateTime RequiredDate { get; set; }
+        [DisplayName("出貨日期")]
+        public DateTime ShippedDate { get; set; }
+
+        [DisplayName("出貨編號")]
+        public int ShipperID { get; set; }
+        public Decimal Freight { get; set; }
+        [DisplayName("出貨地址")]
+        public Char[] ShipAddress { get; set; }
+        [DisplayName("出貨城市")]
+        public Char[] ShipCity { get; set; }
+        [DisplayName("出貨地區")]
+        public Char[] ShipRegion { get; set; }
+        [DisplayName("郵遞區號")]
+        public Char[] ShipPostalCode { get; set; }
+        [DisplayName("出貨國家")]
+        public Char[] ShipCountry { get; set; }
+
+        public List<Orders> GetOrder()
+        {
+            var Order = new List<Orders>()
+            {
+                new Orders
+                {
+                    OrderID=1,
+                    CustomerID=1,
+                    EmployeeID=1,
+                    OrderDate=2018-01-01,
+                },
+                new Orders
+                {
+
+                }
+            };
+            return Order;
+        }
+
+    }
+}
